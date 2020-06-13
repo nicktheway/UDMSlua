7c39e5089bfcc067bd7c93d250742a6b5f3b631c
nicktheway <nikospa96@hotmail.gr>
2020-06-13 16:03:16 +0100

    Minimal template with function call examples

---
+++ Assets/StreamingAssets/Scripts/00_RoomTemplate/group.lua
@@ -0,0 +1,30 @@
+local UE = CS.UnityEngine
+
+local Nagn = Members.Count
+
+function start()
+    -- Group function call example
+    -- full list of available functions at: https://udms-docs.netlify.app/src-pages-scripting-api-group/
+    Group:ToGridFormation(4, UE.Vector3(-2, 0, -2), 1, 1)
+
+    -- Disable Cinemachine
+    local cinemachineBrain = UE.Camera.main:GetComponent(typeof(CS.Cinemachine.CinemachineBrain))
+    if cinemachineBrain then
+        cinemachineBrain.enabled = false
+    end
+end
+
+function update()
+    for i = 0, Nagn - 1 do
+        individualUpdate(i)
+    end
+end
+
+function individualUpdate(id)
+    -- Placeholder: make all agents face (0, 0, 0)
+    -- Group Member function call example,
+    -- full list at: https://udms-docs.netlify.app/src-pages-scripting-api-group-member
+    local newDir =  Members[id]:DirAgentToPnt(UE.Vector3(0, 0, 0))
+    Members[id]:SetDir(newDir)
+end
+
---
+++ Assets/StreamingAssets/Scripts/00_RoomTemplate/settings.lua
@@ -0,0 +1,6 @@
+scene = 'RoomA'
+
+function setUp()
+    local group = room:InstantiateGroup('NeoMan', 'models/main', 16, 'group', 'group.lua')
+end
+
